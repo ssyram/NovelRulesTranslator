55cbad41cc39f1317c40e4ff91b54014f3c1fa2e
ssyram <liguany126@126.com>
2019-04-02 20:59:12 +0800

    Recover main.cpp.

--- main.cpp
+++ main.cpp
@@ -10,9 +10,5 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-	argc = 3;
-	const char *a[] = { "1", "sample.tsl", "sample.hpp" };
-	argv = a;
-    auto r = run(argc, argv);
-	int i = 5;
+    return run(argc, argv);
 }
